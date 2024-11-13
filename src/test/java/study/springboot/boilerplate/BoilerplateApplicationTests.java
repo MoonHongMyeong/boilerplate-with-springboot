1f0bc953374ed21b8b0c83e8a03c9aff08d4a075
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-13 23:36:27 +0900

    project init

---
+++ src/test/java/study/springboot/boilerplate/BoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
